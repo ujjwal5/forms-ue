adeeb4f04938846b861658f8576531ea8a41d1b3
Ravisanker E <rae@adobe.com>
2024-05-08 14:30:48 +0530

    Make test run against chrome mobile

--- playwright.config.js
+++ playwright.config.js
@@ -1,7 +1,5 @@
 import { defineConfig, devices } from '@playwright/test';
-/**
- * @see https://playwright.dev/docs/test-configuration
- */
+
 export default defineConfig({
   testDir: './test/e2e',
   /* Run tests in files in parallel */
@@ -43,10 +41,10 @@ export default defineConfig({
     // },
 
     /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
+    {
+      name: 'Mobile Chrome',
+      use: { ...devices['Pixel 5'] },
+    },
     // {
     //   name: 'Mobile Safari',
     //   use: { ...devices['iPhone 12'] },
@@ -62,11 +60,4 @@ export default defineConfig({
     //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
     // },
   ],
-
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
 });
