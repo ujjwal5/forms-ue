cbfb8e58ad6ce875365b20ca603be8bb68874cfb
pranay-tippa <146062277+pranay-tippa@users.noreply.github.com>
2024-06-05 12:05:26 +0530

    dForms 14513 (#81)
    FORMS-14513:Reset button test for Playwright
    
    Co-authored-by: Pranay Tippa <ptippa@adobe.com>

--- test/e2e/x-walk/resetButton.spec.js
+++ test/e2e/x-walk/resetButton.spec.js
@@ -1,8 +1,20 @@
 import { test, expect } from '../fixtures.js';
-import { openPage } from '../utils.js';
+import { fillField, openPage } from '../utils.js';
 
 const wizardCount = ".repeat-wrapper fieldset[class='panel-wrapper field-wrapper wizard']";
 const wizardPanelCount = 'ul.wizard-menu-items li.wizard-menu-item';
+const titles = ['Text Input', 'Check Box Group', 'Number Input', 'Radio Button', 'Telephone Input', 'Email Input', 'File Attachment', 'Dropdown', 'Date Input'];
+const fileName = 'empty.pdf';
+const dropDownSelector = 'div.drop-down-wrapper select';
+const inputValues = {
+  textInput: 'adobe',
+  emailInput: 'test@adobe.com',
+  numberInput: '123',
+  dropDown: 'Orange',
+  FilePath: './test/e2e/upload/empty.pdf',
+  dataInput: '2022-12-23',
+};
+
 test.describe('resetButton validation test', () => {
   const testURL = '/drafts/tests/x-walk/wizardvalidation';
   test('resetButton validation on wizard panels', async ({ page }) => {
@@ -32,4 +44,48 @@ test.describe('resetButton validation test', () => {
       ],
     );
   });
+
+  test('Check for reset functionality', async ({ page }) => {
+    const testURL1 = '/drafts/tests/x-walk/resetvalidation';
+    await openPage(page, testURL1);
+    // eslint-disable-next-line no-restricted-syntax
+    for (const title of titles) {
+      // eslint-disable-next-line no-await-in-loop,max-len
+      await fillField(page, title, inputValues);
+    }
+    await page.getByRole('button', { name: 'Reset' }).click();
+    // eslint-disable-next-line no-restricted-syntax
+    for (const title of titles) {
+      // eslint-disable-next-line no-await-in-loop,no-use-before-define
+      await checkIfReset(page, title);
+    }
+  });
+  // eslint-disable-next-line no-shadow
+  const checkIfReset = async (page, ComponentsTitle) => {
+    switch (ComponentsTitle) {
+      case 'Text Input':
+      case 'Email Input':
+      case 'Telephone Input':
+      case 'Date Input':
+      case 'Number Input':
+        expect(await page.getByLabel(ComponentsTitle).inputValue()).toBe('');
+        break;
+      case 'Check Box Group':
+        expect(await page.getByRole('checkbox', { name: 'Item 1' }).isChecked()).toBe(false);
+        break;
+      case 'Radio Button':
+        expect(await page.getByRole('radio', { name: 'Item 2' }).isChecked()).toBe(false);
+        break;
+      case 'Dropdown':
+        // eslint-disable-next-line no-case-declarations
+        await expect(page.locator(dropDownSelector)).toHaveValue('');
+        break;
+      case 'File Attachment':
+        expect(await page.getByLabel(fileName).isVisible()).toBe(false);
+        break;
+      default:
+        console.error(`${ComponentsTitle} Title is not visible`);
+        break;
+    }
+  };
 });
