bb363d5bcbb2b2f9fe483e180d1c41cd488fdef7
ujjgupta <ujjgupta@adobe.com>
2024-07-22 14:08:42 +0530

    Adding rating custom component

--- blocks/form/mappings.js
+++ blocks/form/mappings.js
@@ -3,13 +3,18 @@
  *
  * */
 export default async function componentDecorator(fd) {
-  const { ':type': type = '', fieldType } = fd;
+  const { ':type': type = '', fieldType, name } = fd;
   if (fieldType === 'file-input') {
     const module = await import('./components/file.js');
     return module.default;
   }
   if (type.endsWith('wizard')) {
     const module = await import('./components/wizard.js');
+    return module.default;
+  }
+  // if field type is rating return rating js copmonent
+  if (name === 'rating') {
+    const module = await import('./components/rating.js');
     return module.default;
   }
   return null;
