07723e7efb26568213f20c3052c8cd60474ca21f
Ravisanker E <ravisanker1999@gmail.com>
2024-08-06 21:01:41 +0530

    fix: colspan for repeated panels (#137)
    * fix:colspan in repeated panels
    
    * added e2e test case
    
    ---------
    
    Co-authored-by: Ravisanker E <rae@ravisankers-mbp.macromedia.com>

--- test/e2e/x-walk/repeat.spec.js
+++ test/e2e/x-walk/repeat.spec.js
@@ -8,4 +8,15 @@ test.describe('Repeatability test', () => {
     const childCount = await page.locator('.repeat-wrapper').evaluate((el) => Array.from(el.children).filter((child) => child.classList.contains('panel-wrapper')).length);
     await expect(childCount).toBe(5);
   });
+
+  test('test colspan for repeated panels', async ({ page }) => {
+    await openPage(page, testURL);
+    const elements = await page.$$('main .form form .field-wrapper.col-4');
+    // eslint-disable-next-line no-restricted-syntax
+    for (const element of elements) {
+      // eslint-disable-next-line no-await-in-loop
+      const gridColumn = await element.evaluate((el) => getComputedStyle(el).getPropertyValue('grid-column'));
+      expect(gridColumn).toBe('span 4');
+    }
+  });
 });
