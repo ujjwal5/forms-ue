e308aefff504ca009d3f1135608d942c85c51ea9
Talmiz Ahmed <115221634+TalmizAhmed@users.noreply.github.com>
2024-09-09 18:21:02 +0530

    Fix for Doc based authoring (#162)
    Co-authored-by: Talmiz Ahmed <talmiza@adobe.com>

--- blocks/form/form.js
+++ blocks/form/form.js
@@ -479,7 +479,7 @@ export async function fetchForm(pathname) {
   // get the main form
   let data;
   let path = pathname;
-  if (path.startsWith(window.location.origin)) {
+  if (path.startsWith(window.location.origin) && !path.endsWith('.json')) {
     if (path.endsWith('.html')) {
       path = path.substring(0, path.lastIndexOf('.html'));
     }
